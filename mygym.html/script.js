7a506f4c0a8623004cce10ddc3da57f4a770712c
Taynara <taynara.rsouza@gmai.com>
2026-05-29 15:42:43 -0300

    versao 25/05

---
+++ mygym.html/script.js
@@ -0,0 +1,69 @@
+// MENU MOBILE
+function toggleMenu() {
+document.getElementById("menu").classList.toggle("active");
+}
+
+// MOSTRAR SERVIÇOS
+function mostrarServicos() {
+const services = document.getElementById("services");
+const plans = document.getElementById("plans");
+
+services.classList.add("show");
+plans.classList.remove("show");
+}
+
+// MOSTRAR PLANOS
+function mostrarPlanos() {
+const services = document.getElementById("services");
+const plans = document.getElementById("plans");
+
+plans.classList.add("show");
+services.classList.remove("show");
+}
+
+// BOTÕES
+function startNow() {
+alert("🔥 Você iniciou sua jornada na academia!");
+}
+
+function joinNow() {
+alert("✅ Login realizado com sucesso!");
+}
+
+function cardClick(tipo) {
+alert("Você clicou em: " + tipo);
+}
+
+function planSelect(plano) {
+alert("Plano escolhido: " + plano);
+}
+
+// CARROSSEL
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+slides.forEach(slide => slide.classList.remove("active"));
+slides[index].classList.add("active");
+}
+
+function changeSlide(direction) {
+currentSlide += direction;
+
+if (currentSlide >= slides.length) currentSlide = 0;
+if (currentSlide < 0) currentSlide = slides.length - 1;
+
+showSlide(currentSlide);
+}
+
+// Auto slide
+setInterval(() => {
+changeSlide(1);
+}, 4000);
+
+showSlide(currentSlide);
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  alert("Mensagem enviada com sucesso! ✅");
+}
